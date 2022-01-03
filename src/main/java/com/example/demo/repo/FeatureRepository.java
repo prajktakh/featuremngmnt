46a7351064e780ae6392d63e03294b4109e756dd
prajkta <prajkta.khose@accenture.com>
2022-01-03 17:25:58 +0200

    improved imports

--- src/main/java/com/example/demo/repo/FeatureRepository.java
+++ src/main/java/com/example/demo/repo/FeatureRepository.java
@@ -3,7 +3,6 @@ package com.example.demo.repo;
 import com.example.demo.model.Feature;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 
